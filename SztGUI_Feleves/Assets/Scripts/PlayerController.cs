13ebd7664b6238f52347ef7a0c1d763c90b612a1
Benkő-Hadaró Márton <marton.benko@stud.uni-obuda.hu>
2024-04-06 15:30:07 +0200

    sprites added and working!

--- SztGUI_Feleves/Assets/Scripts/PlayerController.cs
+++ SztGUI_Feleves/Assets/Scripts/PlayerController.cs
@@ -10,10 +10,18 @@ public class PlayerController : MonoBehaviour
     private Vector2 movement;
     private Rigidbody2D rb;
 
+    //sprite animalashoz
+    private Animator myAnimator;
+    private SpriteRenderer mySpriteRenderer;
+
+
+
     private void Awake()
     {
         playerControls = new PlayerControls();
         rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void OnEnable()
@@ -33,12 +41,16 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        AdjustPlayerFacingDirection();
         Move();
     }
 
     private void PlayerInput()
     {
         movement = playerControls.Movement.Move.ReadValue<Vector2>();
+
+        myAnimator.SetFloat("moveX", movement.x);
+        myAnimator.SetFloat("moveY", movement.y);
     }
 
     private void Move()
@@ -46,7 +58,22 @@ public class PlayerController : MonoBehaviour
         rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
     }
 
-    
+    private void AdjustPlayerFacingDirection()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
+
+        if (mousePos.x < playerScreenPoint.x)
+        {
+            mySpriteRenderer.flipX = true;
+        }
+        else
+        {
+            mySpriteRenderer.flipX = false;
+        }
+    }
+
+
 
     IEnumerator Dash()
     {
