cda88041dc2dd1c677ada0f5b6082fb290394365
pbiro03 <p.biro03@stud.uni-obuda.hu>
2024-04-11 13:08:20 +0200

    enemies are always facing to the player
    Since the current skin of the object is not making this possible, I also commented this section out temporary.

--- SztGUI_Feleves/Assets/AIChase.cs
+++ SztGUI_Feleves/Assets/AIChase.cs
@@ -8,18 +8,24 @@ public class AIChase : MonoBehaviour
     public float speed;
 
     private float distance;
-    // Start is called before the first frame update
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
     void Update()
     {
+        //the commented sections are making it possible for the enemy to always face the player.
+        //it is outcommented, since the current skin of the enemy has shadows init, making the turning weird.( in some angles, the shadows are on top of the enemy)
+
         distance = Vector2.Distance(transform.position, player.transform.position);
         Vector2 direction = player.transform.position - transform.position;
+        //direction.Normalize();
+        //float angle= Mathf.Atan2(direction.y, direction.x)*Mathf.Rad2Deg;
 
         transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+        //transform.rotation = Quaternion.Euler(Vector3.forward * angle);
     }
 }
