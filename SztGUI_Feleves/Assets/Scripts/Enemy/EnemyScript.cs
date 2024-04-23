4443341a727e153416d38069dbbe4aa97f482ac0
pbiro03 <p.biro03@stud.uni-obuda.hu>
2024-04-23 10:58:17 +0200

    enemy_script optimalized

--- SztGUI_Feleves/Assets/Scripts/Enemy/EnemyScript.cs
+++ SztGUI_Feleves/Assets/Scripts/Enemy/EnemyScript.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AIChase_WithCoin : MonoBehaviour
+public class EnemyScript : MonoBehaviour
 {
     //public GameObject player;
     public Transform player;
     public float speed;
-    [SerializeField] private GameObject goinCoinPrefab;
+    [SerializeField] private GameObject onDestroyPrefab;
     private float distance;
 
     void Start()
@@ -18,20 +18,14 @@ public class AIChase_WithCoin : MonoBehaviour
 
     void Update()
     {
-        //get the target
         if(!player)GetTarget();
 
 
-        //the commented sections are making it possible for the enemy to always face the player.
-        //it is outcommented, since the current skin of the enemy has shadows init, making the turning weird.( in some angles, the shadows are on top of the enemy)
-
         distance = Vector2.Distance(transform.position, player.transform.position);
         Vector2 direction = player.transform.position - transform.position;
-        //direction.Normalize();
-        //float angle= Mathf.Atan2(direction.y, direction.x)*Mathf.Rad2Deg;
 
         transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
-        //transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+
     }
     private void GetTarget()
     {
@@ -39,7 +33,6 @@ public class AIChase_WithCoin : MonoBehaviour
     }
     private void OnDestroy()
     {
-        
-            Instantiate(goinCoinPrefab, transform.position, Quaternion.identity);
+            Instantiate(onDestroyPrefab, transform.position, Quaternion.identity);
     }
 }
